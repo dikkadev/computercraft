be2103e759ed6e4cc0e1afde8955a4fd86b4fb7a
sett <raik.rohde@gmail.com>
2023-05-23 03:31:44 +0200

    feat: add certusQuartz mining feature
    Added a Lua script to mine fully grown AE2 certus quartz clusters using a turtle. The script waits for the cluster to fully grow before mining and sleeps for 5 seconds before checking again.

---
+++ certusQuartz.lua
@@ -0,0 +1,14 @@
+while true do
+    success, block = turtle.inspect()
+
+    if success then
+        if block.name == "ae2:quartz_cluster" then
+            print('Fully grown, mining...')
+            turtle.dig()
+        else
+            print('Not fully grown, waiting...')
+        end
+    end
+
+    os.sleep(5)
+end
